10687935057fbe9e35bc70fb863a2250f8dddfba
Hassan Nurudeen <nurudeenbika@gmail.com>
2024-07-13 15:21:47 +0100

    feat: added 1.13 solution

--- Part1/anecdotes/src/App.jsx
+++ Part1/anecdotes/src/App.jsx
@@ -1,54 +1,52 @@
-const Header = ({ course }) => {
-  
-  return (
-    <>
-     <h1>{course}</h1>
-    </>
-  ) 
-}
+import { useState } from 'react'
 
-const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
-  return (
-    <>
-      <p>
-      {part1} {exercises1}
-    </p>
-    <p>
-      {part2} {exercises2}
-    </p>
-    <p>
-      {part3} {exercises3}
-    </p>
-    </>
+const Button = ({ handleClick, text }) => (<button onClick={handleClick}>
+  {text}
+  </button>
   )
-}
 
-const Total = ({exercises1=10, exercises2=7, exercises3=14}) => {
+const VoteCounts = ({ text, votes }) => {
   return (
     <>
-<p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <p>{text}</p>
+      <p>has {votes}</p>
     </>
   )
 }
 
 const App = () => {
- const course = 'Half Stack application development'
- const part1 = 'Fundamentals of React'
- const exercises1 = 10
- const part2 = 'Using props to pass data'
- const exercises2 = 7
- const part3 = 'State of a component'
- const exercises3 = 14
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
 
- return (
-  <div>
-    <Header course={course} />
-    <Content part1={part1} exercises1={exercises1} />
-    <Content part2={part2} exercises2={exercises2} />
-    <Content part3={part3} exercises3={exercises3} />
-    <Total /> 
-  </div>
- )
+  const setToSelected = () => {
+    const randomint = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomint)
+}
+  
+  const setToVotes = () => {
+    const updatedVotes = [...votes]
+    updatedVotes[selected] += 1
+    setVotes(updatedVotes)
+  }
+
+  return (
+    <div>
+      <VoteCounts text={anecdotes[selected]} votes={votes[selected]} />
+      <Button handleClick={setToVotes} text='votes' />
+      <Button handleClick={setToSelected} text='next anecdotes' />
+    </div>
+  )
 }
 
 export default App
