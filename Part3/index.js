11ea540aa8f2116c35f0637d23873a2465297068
Hassan Nurudeen <nurudeenhassan862@gmail.com>
2024-08-17 15:34:32 +0100

    feat: added 3.17* solution

--- Part3/index.js
+++ Part3/index.js
@@ -69,11 +69,26 @@ app.post('/api/persons', (request, response) => {
   } 
   const person = new Person({
     name: body.name,
-    number: String(body.number)
+    number: body.number
   })
   person.save().then(savePerson => {
     response.json(savePerson)
   })
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
