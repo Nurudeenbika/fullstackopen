f97a440d98eafab58ec558f9bac54179541cf7eb
Hassan Nurudeen <nurudeenhassan862@gmail.com>
2024-08-17 15:43:34 +0100

    feat: added 3.18* solution

--- Part3/index.js
+++ Part3/index.js
@@ -52,7 +52,7 @@ morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.post('/api/persons', (request, response) => {
-  body = request.body
+  body = request.body;
 
  
   if (!body.name || !body.number === undefined) {
@@ -72,7 +72,7 @@ app.post('/api/persons', (request, response) => {
     number: body.number
   })
   person.save().then(savePerson => {
-    response.json(savePerson)
+    response.json(savePerson);
   })
 })
 
@@ -86,9 +86,9 @@ app.put('/api/persons/:id', (request, response, next) => {
 
   Person.findByIdAndUpdate(request.params.id, person, { new: true })
     .then(updatedPerson => {
-      response.json(updatedPerson)
+      response.json(updatedPerson);
     })
-    .catch(error => next(error))
+    .catch(error => next(error));
 })
 
 const unknownEndpoint = (request, response) => {
@@ -97,7 +97,7 @@ const unknownEndpoint = (request, response) => {
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
-    response.json(persons)
+    response.json(persons);
   })
 })
 
@@ -106,23 +106,23 @@ app.get('/api/persons/:id', (request, response, next) => {
     if (person) {
       response.json(person)
     } else {
-      response.status(404).end()
+      response.status(404).end();
     }
   })
-  .catch(error => next(error))
+  .catch(error => next(error));
 })
 
 app.delete('/api/persons/:id', (request, response) => {
   Person.findByIdAndDelete(request.params.id)
     .then(result => {
-      response.status(204).end()
+      response.status(204).end();
     })
     .catch(error => next(error))
 })
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
+    returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id
     delete returnedObject.__v
   }
@@ -135,7 +135,7 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: 'malformatted id' })
   }
 
-  next(error)
+  next(error);
 }
 
 app.get('/', (request, response) => {
@@ -147,10 +147,10 @@ app.get('/info', (request, response) => {
   response.send(`Phonebook has info for 2 people <p> ${currentDate}</p>`)
 })
 
-app.use(unknownEndpoint)
-app.use(errorHandler)
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`);
 })
