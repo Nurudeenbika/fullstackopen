be87add823ba182578df7c91b16794d63794ff83
Hassan Nurudeen <nurudeenbika@gmail.com>
2024-11-23 21:14:50 +0100

    feat: add Part9 9.1

---
+++ Part9/exerciseCalculator.ts
@@ -0,0 +1,43 @@
+interface ExerciseResult {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const calculatExercises = (dailyHours: number[], target: number): ExerciseResult => {
+  const periodLength = dailyHours.length;
+  const trainingDays = dailyHours.filter(day => day > 0).length;
+  const average = dailyHours.reduce((sum, hours) => sum + hours, 0) / periodLength;
+  const success = average >= target;
+
+  let rating: number;
+  let ratingDescription: string;
+
+
+  if (average >= target) {
+    rating = 3;
+    ratingDescription = "great job, you met your target!";
+  } else if (average >= target * 0.75) {
+    rating = 2;
+    ratingDescription = "not too bad but could be better";
+  } else {
+    rating = 1;
+    ratingDescription = "you need to put in more effort";
+  }
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average
+  };
+};
+
+console.log(calculatExercises([3, 0, 2, 4.5, 0, 3, 1 ], 2))
